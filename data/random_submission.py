bbe9b9691c7ccaab33e3f7c7863004231f09e6d4
Isaac <isaacchu888@gmail.com>
2025-09-18 17:54:56 -0500

    remove temporary random submission code

--- data/random_submission.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-import numpy as np
-
-sample_submission_path = os.path.abspath("data/sample_submission.csv")
-df = pd.read_csv(sample_submission_path, dtype={"accession": str}, usecols=["accession"])
-rng = np.random.default_rng(seed=0)
-df["score"] = rng.normal(loc=0.731701, scale=4.619144, size=len(df))
-df.to_csv("data/random_submission.csv", index=False)
