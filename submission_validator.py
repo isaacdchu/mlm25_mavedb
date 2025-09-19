6eb5fd9a5578b6f2d2a3a1517706e7f23b228fe6
Isaac <isaacchu888@gmail.com>
2025-09-19 00:30:44 -0500

    fixed linting for submission_validator.py

--- submission_validator.py
+++ submission_validator.py
@@ -10,34 +10,32 @@ It checks for:
 7. Valid and in-order accessions in "accession" column as per "data/sample_submission.csv"
 '''
 
-from sys import argv
+import sys
 from pathlib import Path
 import pandas as pd
 
 def validate(submission: str) -> tuple[bool, str]:
-    """ Validates a submission file.
-
+    """
+    Validates a submission file.
     The submission file must meet the aforementioned criteria to be considered valid.
-    If any of the criteria are not met, the function returns False and a message indicating the issues.
-
+    If any of the criteria are not met, returns False and a message indicating the issues.
     Args:
         submission (str): Path to the submission file.
     Returns:
         tuple[bool, str]: A tuple containing a boolean indicating success or failure,
                             and a message string with details about the validation.
     """
-
     success: bool = True
     message: str = ""
 
     # Verify file path
     try:
         submission_path: Path = Path(submission).resolve()
-    except Exception as e:
+    except OSError as e:
         success = False
         message += f"Error resolving submission path: {e}\n"
         return (success, message)
-    
+
     # Verify file type
     if not submission_path.exists():
         success = False
@@ -50,7 +48,7 @@ def validate(submission: str) -> tuple[bool, str]:
         message += f"Submission path is not a CSV file: {submission_path}\n"
     if not success:
         return (success, message)
-    
+
     # Verify file content
     try:
         df = pd.read_csv(
@@ -62,11 +60,13 @@ def validate(submission: str) -> tuple[bool, str]:
         correct_shape = (118421, 2)
         if df.shape != correct_shape:
             success = False
-            message += f"Submission file has incorrect shape: {df.shape}, expected {correct_shape}\n"
+            message += f"Submission file has incorrect shape: \
+            {df.shape}, expected {correct_shape}\n"
         correct_columns = ("accession", "score")
         if tuple(df.columns) != correct_columns:
             success = False
-            message += f"Submission file has incorrect columns: {tuple(df.columns)}, expected {correct_columns}\n"
+            message += f"Submission file has incorrect columns: \
+                {tuple(df.columns)}, expected {correct_columns}\n"
         if not all(isinstance(score, (float, int)) for score in df["score"]):
             success = False
             message += "Submission file 'score' column contains non-numeric values\n"
@@ -81,8 +81,9 @@ def validate(submission: str) -> tuple[bool, str]:
         # Check order of accessions
         if any(accessions["accession"].iloc[i] != df["accession"].iloc[i] for i in range(len(df))):
             success = False
-            message += "Submission file 'accession' column contains invalid/out-of-order accession(s)\n"
-    except Exception as e:
+            message += "Submission file 'accession' column contains \
+                invalid/out-of-order accession(s)\n"
+    except (FileNotFoundError, pd.errors.ParserError, ValueError) as e:
         success = False
         message += f"Error reading CSV file: {e}\n"
         return (success, message)
@@ -90,16 +91,14 @@ def validate(submission: str) -> tuple[bool, str]:
     return (success, message)
 
 def main(*args) -> None:
-    """ Main function for validating submission files.
-
+    """ 
+    Main function for validating submission files.
     Multiple submission files can be validated in one run through command line arguments.
-
     Args:
         *args: Variable length argument list of submission file paths.
     Returns:
         None
     """
-    
     print("----------------------------------------------------------------------")
     for submission in args:
         print(f"\tValidating submission:\t{submission}")
@@ -111,7 +110,7 @@ def main(*args) -> None:
         else:
             print(f"\tValidation succeeded:\t{submission}")
         print("----------------------------------------------------------------------")
-    exit(0)
+    sys.exit(0)
 
 if __name__ == "__main__":
-    main(*argv[1:])
+    main(*sys.argv[1:])
