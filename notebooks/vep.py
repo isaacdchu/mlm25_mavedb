039903a49f0fcfba922138c4ca584dd15fdd9882
Isaac <isaacchu888@gmail.com>
2025-09-21 20:36:11 -0500

    multithreading

--- notebooks/vep.py
+++ notebooks/vep.py
@@ -3,9 +3,11 @@ Script to save all VEP data to pickle file in batches of 100.
 """
 
 import sys
+import time
 from pathlib import Path
 import logging
 import pickle
+from concurrent.futures import ThreadPoolExecutor, Future
 import pandas as pd
 from requests import Timeout
 from utils import get_vep_data
@@ -22,6 +24,60 @@ def save_batch(vep_batch: list[dict], save_path: Path) -> None:
     with open(save_path, "ab") as f:
         pickle.dump(vep_batch, f)
     logging.info("Saved successfully")
+
+def process_batch(start_index: int, end_index: int, ensp: pd.Series, pos: pd.Series, alt_long: pd.Series) -> list[dict]:
+    """
+    Process a batch of VEP data.
+    Args:
+        start_index (int): The index of the data in the original DataFrame
+        end_index (int): The ending index of the data in the original DataFrame
+        ensp (str): Ensembl protein ID
+        pos (int): Position of the variant
+        alt_long (str): Alternate amino acid
+    Returns:
+        list[dict]: A list of VEP data dictionaries
+    """
+    vep_batch: list[dict] = []
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures: list[Future] = []
+        for i in range(start_index, end_index):
+            if (i % 4 == 3):
+                time.sleep(2)
+            futures.append(executor.submit(get_vep_data_threaded, i, ensp.iat[i], pos.iat[i], alt_long.iat[i]))
+    sorted_results = sorted([future.result() for future in futures], key=lambda x: x[0])
+    for result in sorted_results:
+        vep_batch.append(result[1])
+    return vep_batch
+
+def get_vep_data_threaded(index: int, ensp: str, pos: int, alt_long: str) -> tuple[int, dict]:
+    """
+    Fetch VEP data in a thread and return the result with its index.
+    Args:
+        index (int): The index of the data in the original DataFrame
+        ensp (str): Ensembl protein ID
+        pos (int): Position of the variant
+        alt_long (str): Alternate amino acid
+    Returns:
+        tuple(int, dict): A tuple containing the index and the fetched VEP data dictionary
+        index is returned as -1 if there was an error fetching the data
+        index is returned as -2 if there was timeout or connection error
+    """
+    retries_left: int = 5
+    while retries_left > 0:
+        try:
+            vep_data: dict = get_vep_data(ensp, pos, alt_long)
+            if (retries_left < 5):
+                logging.info("Successfully fetched VEP data for index %d after retrying", index)
+            return (index, vep_data)
+        except (Timeout, ConnectionError) as e:
+            logging.error("Connection error fetching VEP data for index %d: %s", index, e)
+            retries_left -= 1
+            if retries_left == 0:
+                break
+        except Exception as e:
+            logging.error("Error fetching VEP data for index %d: %s", index, e)
+            return (-1, {})
+    return (-2, {})
 
 def main(*args) -> None:
     """
@@ -56,25 +112,15 @@ def main(*args) -> None:
     try:
         vep_batch: list[dict] = []
         while (i < len(df)):
-            print(f"{i}/{len(df)} ", end="")
-            vep_batch.append(
-                get_vep_data(
-                    df["ensp"].iat[i],
-                    df["pos"].iat[i],
-                    df["alt_long"].iat[i]
-                )
-            )
-            i += 1
-            if (i % 100 == 0):
-                logging.info("Saving index %d through %d.", last_index_save, i - 1)
-                last_index_save = i - 1
-                save_batch(vep_batch, save_path)
-                vep_batch.clear()
+            end_index = min(i + 100, len(df)) # this index is exclusive
+            logging.info("Processing batch: %d-%d/%d", i, end_index, len(df))
+            vep_batch = process_batch(i, end_index, df["ensp"], df["pos"], df["alt_long"])
+            i += 100
+            logging.info("Saving index %d through %d.", last_index_save, end_index - 1)
+            last_index_save = i - 1
+            save_batch(vep_batch, save_path)
+            vep_batch.clear()
             print()
-        logging.info("Saving index %d through %d.", last_index_save, i - 1)
-        last_index_save = i - 1
-        save_batch(vep_batch, save_path)
-        vep_batch.clear()
         logging.info("All done! Processed up through index %d.", last_index_save)
         sys.exit(0)
     except KeyboardInterrupt:
