f46787e820166835670c388665189f5eaec7c7bd
Isaac <isaacchu888@gmail.com>
2025-09-26 01:07:47 -0500

    esm file name changes to fix imports

---
+++ notebooks/esmc.py
@@ -0,0 +1,28 @@
+"""
+Helper script for getting all ESM C embeddings for a given dataaset
+"""
+import sys
+import pandas as pd
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, Future
+from torch import Tensor
+import pickle
+
+import utils
+
+def main(*argv) -> None:
+    """
+    Main function to get ESM C embeddings for the training dataset and save them.
+    """
+    if (len(argv) != 3):
+        print("Usage: python get_esm_c_embeddings.py <path/to/csv> <path/to/save/embeddings.pkl> <start_index>")
+        sys.exit(1)
+    csv_path: Path = Path(argv[0])
+    save_path: Path = Path(argv[1])
+    start_index: int = int(argv[2])
+
+
+    return
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
