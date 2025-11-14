559f7922650342e1ca50a39b6d9d8918728d7246
Schnooodle <isaacchu888@gmail.com>
2025-11-14 02:36:46 -0600

    code skeleton

---
+++ src/aasp/models/example_model.py
@@ -0,0 +1,2 @@
+class ExampleModel:
+    pass
--- training/src/aasp/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_handler.data_handler import DataHandler
-from .model.model import Model
-from .model.trainer import Trainer
-from .model.evaluator import Evaluator
